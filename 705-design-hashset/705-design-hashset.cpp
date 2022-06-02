e7bb36869e492fbca83b34906053517cfdf31fcc
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-03 00:29:22 +0530

    Time: 452 ms (7.50%), Space: 40.1 MB (90.73%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,45 @@
 class MyHashSet {
 public:
-    int arr[1000001];
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(NULL) {}
+        ListNode(int x) : val(x), next(NULL) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    
+    ListNode* head;
+    
     MyHashSet() {
-        fill(arr, arr+1000001, 0);
+        head = new ListNode(-1);
     }
     
     void add(int key) {
-        arr[key] = 1;
+        ListNode* temp = new ListNode(key);
+        ListNode* curr = head;
+        while(curr and curr->next) {
+            if(curr->next->val == key)    return;
+            curr = curr->next;
+        }
+        curr->next = temp;
     }
     
     void remove(int key) {
-        arr[key] = 0;
+        ListNode* temp = head;
+        while(temp and temp->next) {
+            if(temp->next->val == key)
+                temp->next = temp->next->next;
+            temp = temp->next;
+        }
     }
     
     bool contains(int key) {
-        return arr[key];
+        ListNode* temp = head;
+        while(temp) {
+            if(temp->val == key)    return true;
+            temp = temp->next;
+        }
+        return false;
     }
 };
 
