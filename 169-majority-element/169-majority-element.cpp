e13d5b293199465b59dbaef0af296f2e9c56e777
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-10 14:40:47 +0530

    Time: 33 ms (29.08%), Space: 19.5 MB (59.51%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,14 +5,15 @@ public:
         int el = nums[0];
         int count = 1;
         for(int i = 1; i < nums.size(); i++) {
+            
+            if(nums[i] == el)
+                count++;
+            else
+                count--;
             if(count == 0) {
                 el = nums[i];
                 count = 1;
             }
-            else if(nums[i] == el)
-                count++;
-            else
-                count--;
         }
         
         return el;
