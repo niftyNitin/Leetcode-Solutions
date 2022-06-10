baa3ad3029d4ebc8fb3201bd8f2d36fa69f64937
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-10 15:41:55 +0530

    Added solution - LeetHub

---
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+class Solution{
+    int minEle = INT_MAX;
+    stack<int> s;
+    stack<int> ss;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           if(s.empty())    return -1;
+           return ss.top();
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           if(s.empty())    return -1;
+           int x = s.top();
+           s.pop();
+           if(x == ss.top())    ss.pop();
+           return x;
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           s.push(x);
+           if(ss.empty()) ss.push(x);
+           else if(ss.top() >= x)   ss.push(x);
+       }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        Solution ob;
+        while(q--){
+           int qt;
+           cin>>qt;
+           if(qt==1)
+           {
+              //push
+              int att;
+              cin>>att;
+              ob.push(att);
+           }
+           else if(qt==2)
+           {
+              //pop
+              cout<<ob.pop()<<" ";
+           }
+           else if(qt==3)
+           {
+              //getMin
+              cout<<ob.getMin()<<" ";
+           }
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
