e6110ba81834bca4ebfa0d8e8818e7b1c00b5f4d
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-17 01:23:49 +0530

    Added solution - LeetHub

--- Smallest distinct window - GFG/smallest-distinct-window.cpp
+++ Smallest distinct window - GFG/smallest-distinct-window.cpp
@@ -6,29 +6,31 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int findSubString(string str)
+    int findSubString(string s)
     {
         // Your code goes here
         unordered_map<char,int> req;
-        for(auto c:str) req[c]=1;
+        for(auto c:s) req[c]=1;
         
-        int start=0,end=0;
+        int head = -1, tail = 0;
         int ans=INT_MAX;
         int target=req.size();
         int count=0;
+        req.clear();
         
-        while(end<str.length()){
-            if(req[str[end]]>0){
-                count++;
+        while(tail < s.size()) {
+            while(head+1 < s.size() and count < target) {
+                req[s[++head]]++;
+                if(req[s[head]] == 1)   count++;
             }
-            req[str[end]]--;
-            
-            if(count==target){
-                while(start<=end and req[str[start]]<0) req[str[start]]++,start++;
+            if(count == target)
+                ans = min(ans, head-tail+1);
                 
-                ans=min(ans,end-start+1);
+            if(head >= tail) {
+                req[s[tail]]--;
+                if(req[s[tail]] == 0)   count--;
+                tail++;
             }
-            end++;
         }
         return ans;
     }
