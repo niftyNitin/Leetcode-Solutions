74297b5cdcdb62a9e104396128cc0d606bb2cd44
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-28 16:51:06 +0530

    Time: 745 ms (19.74%), Space: 13 MB (6.12%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums) {
-        if(nums.size() < 3)     return 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int count = 0;
-        for(int i = 2; i < n; i++) {
-            int l = 0, r = i-1;
-            while(l < r) {
-                int sum_two_sides = nums[l] + nums[r];
-                if(sum_two_sides > nums[i]) {
-                    count += (r-l);
-                    r--;
-                }
-                else l++;
+   int findIdx(int lo, int hi, int target, vector<int> &nums){
+        while(lo <= hi){
+            int mid = (lo + hi) / 2;
+            
+            if(nums[mid] >= target){
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
             }
         }
-        return count;
+        
+        return hi;
+    }
+    
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        
+        // 3 sides of triangle are valid if a + b > c
+        // We can select a & b using 2 for loops
+        // To check the third side, we first sort the given array
+        // Since c > a && c > b, we definitely know that c will lie beyond b's index
+        // So, we search for c such that it is the largest value less than (a + b) and return its index
+        // From the b's index upto this index will be the choices we can have for our side
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int idx = findIdx(j + 1, n - 1, nums[i] + nums[j], nums);
+                
+                int k = idx - j;
+                ans += k;
+            }
+        }
+        
+        return ans;
     }
 };
