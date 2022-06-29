66ddd0a2a9fda9f5e53845dbdc33885156f12759
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-29 10:50:48 +0530

    Time: 64 ms (69.16%), Space: 31.6 MB (74.37%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_heap;
-        for(auto element : arr) {
-            int diff = abs(element - x);
-            min_heap.push({diff, element});
-        }
+        int low = 0, high = arr.size()-1;
+        int n = arr.size();
+        // while(low <= high) {
+        //     int mid = low + (high-low) / 2;
+        //     if(arr[mid] > x)    high = mid-1;
+        //     else    low = mid;
+        // }
+        int r = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int l = r-1;
         vector<int> res;
-        while(k--) {
-            res.push_back(min_heap.top().second);
-            min_heap.pop();
+        while(l >= 0 or r < n) {
+            if(res.size() == k) break;
+            int diff1 = INT_MAX, diff2 = INT_MAX;
+            if(l >= 0)   diff1 = abs(arr[l] - x);
+            if(r < n)   diff2 = abs(arr[r] - x);
+            if(diff1 <= diff2)   res.push_back(arr[l]), l--;
+            else    res.push_back(arr[r]), r++;
         }
         sort(res.begin(), res.end());
         return res;
