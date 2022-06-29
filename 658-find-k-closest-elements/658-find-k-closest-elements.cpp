366653034157a505c8a360b33132b9713aa025b9
Nitin Gupta <gupta10nitin99@gmail.com>
2022-06-29 11:15:55 +0530

    Time: 51 ms (85.22%), Space: 31.9 MB (50.70%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int low = 0, high = arr.size()-1;
-        int n = arr.size();
-        // while(low <= high) {
-        //     int mid = low + (high-low) / 2;
-        //     if(arr[mid] > x)    high = mid-1;
-        //     else    low = mid;
-        // }
-        int r = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
-        int l = r-1;
-        vector<int> res;
-        while(l >= 0 or r < n) {
-            if(res.size() == k) break;
-            int diff1 = INT_MAX, diff2 = INT_MAX;
-            if(l >= 0)   diff1 = abs(arr[l] - x);
-            if(r < n)   diff2 = abs(arr[r] - x);
-            if(diff1 <= diff2)   res.push_back(arr[l]), l--;
-            else    res.push_back(arr[r]), r++;
+    vector<int> findClosestElements(vector<int>& nums, int k, int x) {
+        int start = 0, end = nums.size()-1;
+        while(start < end and end-start+1 != k) {
+            int diff1 = abs(nums[start] - x);
+            int diff2 = abs(nums[end] - x);
+            
+            if(diff1 <= diff2)  end--;
+            else    start++;            
         }
-        sort(res.begin(), res.end());
+        vector<int> res;
+        for(int i = start; i <= end; i++) res.push_back(nums[i]);
         return res;
     }
 };
