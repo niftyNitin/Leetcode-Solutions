0c3ef582950b3d73f85c13159c5428e0d88d5e34
Nitin Gupta <gupta10nitin99@gmail.com>
2022-07-09 00:17:27 +0530

    Time: 25 ms (56.10%), Space: 9.2 MB (60.77%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int count = 0;
-
-    
-    int dp[1001][1001][2];
+    int dp[22][1001][2];
     int helper(vector<int> &nums, int target, int i, int sum) {
         
-        if(i == nums.size() and sum == target) {
-           
+        if(i == nums.size() and sum == target)
             return 1;
-            
         
-        }
+        if(i >= nums.size())
+            return 0;
+    
         
-        if(i >= nums.size())    return 0;
+        if(sum >=0  && dp[i][abs(sum)][0] != -1)    return dp[i][abs(sum)][0];
         
+        if(sum < 0 && dp[i][abs(sum)][1] != -1)    return dp[i][abs(sum)][1]; 
         
-       if(sum >=0  && dp[i][abs(sum)][0] != -1)    return dp[i][abs(sum)][0];
-       if(sum < 0 && dp[i][abs(sum)][1] != -1)    return dp[i][abs(sum)][1]; 
-        
-        int a = 0,b=0;
-       a += helper(nums, target, i+1, sum+nums[i]);
-       b += helper(nums, target, i+1, sum-nums[i]);
+        int a =  helper(nums, target, i+1, sum+nums[i]);
+        int b = helper(nums, target, i+1, sum-nums[i]);
         
         if(sum >= 0)
-        return dp[i][abs(sum)][0] =  a + b;
-        else
+            return dp[i][abs(sum)][0] =  a + b;
         return dp[i][abs(sum)][1] = a + b;     
     }
-    
-    
     
     int findTargetSumWays(vector<int>& nums, int target) {
         memset(dp,-1,sizeof(dp));
         return helper(nums, target, 0, 0);
-        
     }
 };
