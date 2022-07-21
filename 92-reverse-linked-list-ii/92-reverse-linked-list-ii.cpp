4b3b82febc2924e929ece9a013cc6a428fd1781d
Nitin Gupta <gupta10nitin99@gmail.com>
2022-07-21 12:17:59 +0530

    Time: 6 ms (30.21%), Space: 7.4 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -11,31 +11,32 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        // inserting a dummy node before the head to avoid egde case where left=1;
-        ListNode* dummy = new ListNode();
-        dummy->next = head;
-        ListNode* prev = dummy;
-        ListNode* curr = head;
-        
-        // shifting the curr pointer to left index
-        for(int i = 1; i < left; i++) {
-            prev = curr;
-            curr = curr->next;
-        }
-        
-        // reversing from left to right
+        ListNode* nhead = new ListNode();
+        nhead->next = head;
+        ListNode* lft = nhead->next;
+        ListNode* prev = nhead;
+        int i = 1;
+        while(i < left) {
+            i++;
+            prev = prev->next;
+            lft = lft->next;
+        } 
+        ListNode* curr = lft;
         ListNode* p = NULL;
-        ListNode* n = NULL;
-        ListNode* c = curr;
-        for(int i = left; i <= right; i++) {
-            n = c->next;
-            c->next = p;
-            p = c;
-            c = n;
+        ListNode* nxt = NULL;
+        
+        
+        while(i <= right) {
+            i++;
+            nxt = curr->next;
+            cout << curr->val << " ";
+            curr->next = p;
+            p = curr;
+            curr = nxt;
         }
         
         prev->next = p;
-        curr->next = n;
-        return dummy->next;
+        lft->next = curr;
+        return nhead->next;
     }
 };
