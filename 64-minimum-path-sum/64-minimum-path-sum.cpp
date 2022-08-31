fde5180ca2216646083a6d1bfa9b6d6b19398421
Nitin Gupta <gupta10nitin99@gmail.com>
2022-08-31 12:35:31 +0530

    Time: 16 ms (49.59%), Space: 10.1 MB (30.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, int i, int j, int row, int col, vector<vector<int>> &dp) {
-        if(i == row-1 and j == col-1)   return grid[i][j];
-        if(i >= row or j >= col)    return 1e5;
-        if(dp[i][j] != -1)  return dp[i][j];
-        int path1 = helper(grid, i+1, j, row, col, dp);
-        int path2 = helper(grid, i, j+1, row, col, dp);
-        return dp[i][j] = grid[i][j] + min(path1, path2);        
-    }
-    
     int minPathSum(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        vector<vector<int>> dp(row, vector<int> (col, -1));
-        return helper(grid, 0, 0, row, col, dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 200));
+        dp[m-1][n-1] = grid[m-1][n-1];
+        
+        // last row in dp array
+        for(int i = m-2; i >= 0; i--) {
+            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+        }
+        
+        // last col in dp array
+        for(int i = n-2; i >= 0; i--){
+            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1];
+        }
+        
+        for(int i = m-2; i >= 0; i--) {
+            for(int j = n-2; j >= 0; j--) {
+                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
