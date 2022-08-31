1dec8693fca7eea3f90f13bd0e75b0f47d201ec8
Nitin Gupta <gupta10nitin99@gmail.com>
2022-08-31 11:43:53 +0530

    Time: 255 ms (43.34%), Space: 17.1 MB (39.10%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        dp[n-1] = 0;
+        vector<int> dp(n, 0);
         for(int i = n-2; i >= 0; i--) {
             int mini = 1e5;
             for(int j = 1; j <= nums[i]; j++) {
-                if(i+j < n and dp[i+j]!= -1) {
+                if(i+j < n) {
                     mini = min(mini, dp[i+j]);
                 }
             }
