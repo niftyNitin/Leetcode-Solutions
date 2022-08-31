9da6e43133b36b154e9234177dd460a8d69c7eeb
Nitin Gupta <gupta10nitin99@gmail.com>
2022-08-31 12:37:26 +0530

    Time: 4 ms (98.70%), Space: 9.7 MB (91.90%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, 200));
-        dp[m-1][n-1] = grid[m-1][n-1];
-        
-        // last row in dp array
-        for(int i = m-2; i >= 0; i--) {
-            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
-        }
-        
-        // last col in dp array
-        for(int i = n-2; i >= 0; i--){
-            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1];
-        }
-        
-        for(int i = m-2; i >= 0; i--) {
-            for(int j = n-2; j >= 0; j--) {
-                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
+        int m = grid.size(), n = grid[0].size();
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(i == m-1 and j == n-1) continue;
+                else if(i != m-1 and j == n-1)
+                    grid[i][j] += grid[i+1][j];
+                else if(i == m-1 and j != n-1)
+                    grid[i][j] += grid[i][j+1];
+                else
+                    grid[i][j] += min(grid[i+1][j], grid[i][j+1]);
             }
         }
-        
-        return dp[0][0];
+        return grid[0][0];
     }
 };
