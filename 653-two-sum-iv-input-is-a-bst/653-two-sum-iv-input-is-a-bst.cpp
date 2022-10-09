32f25e6712e451d5c642b7101dc138281796b833
Nitin Gupta <gupta10nitin99@gmail.com>
2022-10-09 17:05:13 +0530

    Time: 23 ms (99.96%), Space: 36.1 MB (94.70%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,6 +1,6 @@
 /**
  * Definition for a binary tree node.
- * mpruct TreeNode {
+ * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
@@ -11,23 +11,21 @@
  */
 class Solution {
 public:
-    void setInsertion(TreeNode* root, unordered_map<int, TreeNode*> &mp) {
-        if(!root)   return;
-        mp[root->val] = root;
-        setInsertion(root->left, mp);
-        setInsertion(root->right, mp);
+    bool searchBST(TreeNode* curr, int diff, TreeNode* root) {
+        if(!root)   return false;
+        if(root->val == diff and curr != root)   return true;
+        if(diff < root->val)    return searchBST(curr, diff, root->left);
+        else    return searchBST(curr, diff, root->right);
     }
     
-    bool findSum(TreeNode* root, int &k, unordered_map<int, TreeNode*> &mp) {
-        if(!root)   return 0;
-        if(mp.count(k-root->val) and mp[root->val] != mp[k-root->val])
-            return 1;
-        return findSum(root->left, k, mp) or findSum(root->right, k, mp);
+    bool findTargetHelper(TreeNode* curr, int k, TreeNode* root) {
+        if(!curr)   return false;
+        if(searchBST(curr, k-curr->val, root))    return true;
+        return findTargetHelper(curr->left, k, root) or findTargetHelper(curr->right, k, root);
     }
     
     bool findTarget(TreeNode* root, int k) {
-        unordered_map<int, TreeNode*> mp;
-        setInsertion(root, mp);
-        return findSum(root, k, mp);
+        if(!root)   return false;
+        return findTargetHelper(root, k, root);
     }
 };
