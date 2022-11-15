028457811981ea7b75e114cd355a1e240f903a03
Nitin Gupta <gupta10nitin99@gmail.com>
2022-11-15 18:21:52 +0530

    Time: 73 ms (34.25%), Space: 17.5 MB (78.30%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int ans = 0;
+        int i = 0, j = 0;
+        while(i < g.size() and j < s.size()) {
+            if(g[i] <= s[j]) {
+                ans++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
