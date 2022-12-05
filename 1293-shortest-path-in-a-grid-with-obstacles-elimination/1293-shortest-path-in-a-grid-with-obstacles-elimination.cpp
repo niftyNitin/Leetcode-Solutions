7a3554dc20eee9cba0009832b434c1f3eeaa527c
Nitin Gupta <gupta10nitin99@gmail.com>
2022-12-05 15:47:24 +0530

    Time: 552 ms (12.56%), Space: 58.6 MB (9.11%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
-    int dx[4] = {0, 0, 1, -1};
-    int dy[4] = {1, -1, 0, 0};
     int shortestPath(vector<vector<int>>& grid, int k) {
-        vector<vector<int>> vis(grid.size(), vector<int>(grid[0].size(), -1));
-        queue<vector<int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
         
-        //x, y curr path, rem k
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto curr = q.front();
-            int x = curr[0];
-            int y = curr[1];
-            q.pop();
-            
-            if(x < 0 or y < 0 or x >= grid.size() or y >= grid[0].size())
-                continue;
-		    if(x == grid.size()-1 and y == grid[0].size()-1)
-                return curr[2];
-            
-            if(grid[x][y]){
-                if(curr[3]>0)
-                    curr[3]--;
-                else
-                    continue;
+        queue<vector<int>> q;
+        q.push({0,0,k});
+        
+        int dist = 0;
+        int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+        vector<vector<vector<bool>>>vis(n+1, vector<vector<bool>>(m+1, vector<bool>(k+1, false)));
+        vis[0][0][k] = 1;
+        while(!q.empty()) {
+            int s = q.size();
+            while(s--) {
+                auto cell = q.front();
+                q.pop();
+                int x = cell[0];
+                int y = cell[1];
+                int ck = cell[2];
+                
+                if(x == n-1 and y == m-1)   return dist;
+                for(int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    
+                    if(nx < 0 or ny < 0 or nx >= n or ny >= m)  continue;
+                    
+                    if(grid[nx][ny] == 0 and !vis[nx][ny][ck])   {
+                        vis[nx][ny][ck] = true;
+                        q.push({nx, ny, ck});
+                    }
+                    if(grid[nx][ny] == 1 and ck > 0 and !vis[nx][ny][ck-1]) {
+                        vis[nx][ny][ck-1] = true;
+                        q.push({nx, ny, ck-1});
+                    }
+                }
             }
-            
-            if(vis[x][y]!=-1 and vis[x][y] >= curr[3])
-                continue;
-            vis[x][y] = curr[3];
-             
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                q.push({nx, ny, curr[2] + 1, curr[3]});
-            }
+            dist++;
         }
+        
         return -1;
     }
 };
