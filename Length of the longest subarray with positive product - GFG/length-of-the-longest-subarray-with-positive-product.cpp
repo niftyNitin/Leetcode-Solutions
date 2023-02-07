a1da7f617cda5ecabd568c6ffe1a8c2b755b8229
Nitin Gupta <gupta10nitin99@gmail.com>
2023-02-07 23:57:39 +0530

    Added solution - LeetHub

--- Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.cpp
+++ Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.cpp
@@ -16,57 +16,33 @@ class Solution {
         //longest subarray with ppositive product
         int maxLength(vector<int> &arr,int n){
             int i=0,j=0;
-
-           int pro = 1;
-
-           int ans =0;
-
-           int maxi=n;
-
-           while(j<n)
-
-           {
-
-               pro=pro*arr[j];
-
-               if(pro<0)
-
-               {
-
-                   maxi=min(maxi,j);
-
-                   ans=max(ans,j-maxi);
-
-                  pro=-1;
-
-               }
-
-               else if(pro==0)
-
-               {
-
-                   maxi=n;
-
-                   i=j+1;
-
-                   pro=1;
-
-               }
-
-               else if(pro>0){
-
-                   ans=max(ans,j-i+1);
-
-                   pro=1;
-
-               }
-
-               j++;
-
-           }
-
-           return ans;
-
+            int pro = 1;
+            int ans =0;
+            int maxi=n;
+            
+            while(j<n)
+            {
+                pro=pro*arr[j];
+                
+                if(pro<0)
+                {
+                    maxi=min(maxi,j);
+                    ans=max(ans,j-maxi);
+                    pro=-1;
+                }
+                else if(pro==0)
+                {
+                    maxi=n;
+                    i=j+1;
+                    pro=1;
+                }
+                else if(pro>0){
+                    ans=max(ans,j-i+1);
+                    pro=1;
+                }
+                j++;
+            }
+            return ans;
         }
 };
 
