6027d6c7bf71bd4aae53d2c4b4b66c855b62699b
Nitin Gupta <gupta10nitin99@gmail.com>
2023-02-11 02:02:27 +0530

    Time: 67 ms (67.26%), Space: 14.8 MB (94.69%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    static bool cmp(string &a, string &b) {
-        if(a.size() == b.size())    return a < b;
-        return a.size() < b.size();
-    }
+    // static bool cmp(string &a, string &b) {
+    //     if(a.size() == b.size())    return a < b;
+    //     return a.size() < b.size();
+    // }
 public:
     string findLongestWord(string s, vector<string>& dict) {
-        sort(dict.begin(), dict.end(), cmp);
+        // sort(dict.begin(), dict.end(), cmp);
         int len = 0;
         string longest;
         for(auto &d : dict) {
-            if(d.size() == len) continue;
+            // if(d.size() == len) continue;
             int i = 0, j = 0;
             while(i < d.size() and j < s.size()) {
                 if(d[i] == s[j])    i++, j++;
@@ -17,8 +17,13 @@ public:
             }
             
             if(i == d.size()) {
-                len = d.size();
-                longest = d;
+                if(d.size() > len) {
+                    len = d.size();
+                    longest = d;
+                }
+                else if(d.size() == len) {
+                    longest = min(longest, d);
+                }
             }
         }
         return longest;
