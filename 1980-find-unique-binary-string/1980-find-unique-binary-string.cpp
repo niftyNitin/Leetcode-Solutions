024302a5fd81ff9c981527ec01238ead3aaa6298
Nitin Gupta <gupta10nitin99@gmail.com>
2023-02-14 00:01:22 +0530

    Time: 1504 ms (8.74%), Space: 243.3 MB (7.53%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string res = "";
+    void solve(unordered_set<string> &st, string s, int &n) {
+        if(s.size() == n) {
+            if(st.find(s)==st.end())    res = s;
+            return;
+        }
+        
+        s.push_back('0');
+        solve(st, s, n);
+        s.pop_back();
+        s.push_back('1');
+        solve(st, s, n);
+    }
+    
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<string> st(nums.begin(), nums.end());
+        string s = "";
+        int n = nums.size();
+        solve(st, s, n);
+        return res;
+    }
+};
